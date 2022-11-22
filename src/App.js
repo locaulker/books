821ea2ef92b8ead89955a58444f2e1079c73515c
Lawrence <locaulker@gmail.com>
2022-11-22 05:49:17 -0500

    resume @ L88

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import React, { useState } from "react"
+
 function App() {
+  const [books, setBooks] = useState([])
+
+  const createBook = title => {
+    console.log("need to add book with title:", title)
+  }
+
   return <div>App</div>
 }
 
