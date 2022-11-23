9534218cd19be58389e32b4b0b0a51e7d1a149da
Lawrence O. Caulker <locaulker@gmail.com>
2022-11-23 18:30:01 -0500

    added temporary ID generated by math.random

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   const [books, setBooks] = useState([])
 
   const createBook = title => {
-    const updatedBooks = [...books, { id: 123, title: title }]
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title: title },
+    ]
     setBooks(updatedBooks)
   }
 
