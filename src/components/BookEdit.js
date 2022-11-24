e25ceaf6d1e5b8ccd86e539d2f17d194cef7fd82
Lawrence <locaulker@gmail.com>
2022-11-24 01:06:44 -0500

    implemented the BookEdit component

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,7 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 
-function BookEdit() {
-  return <div>BookEdit</div>
+function BookEdit({ book }) {
+  const [title, setTitle] = useState(book.title)
+
+  const handleChange = event => {
+    setTitle(event.target.value)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    console.log("New title is", title)
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="book-edit">
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
+    </form>
+  )
 }
 
 export default BookEdit
