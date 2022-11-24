f4f248967802ae94ba7eccbbe21447cc52eea9b2
Lawrence <locaulker@gmail.com>
2022-11-24 00:00:02 -0500

    added onDelete action

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ import BookList from "./components/BookList"
 function App() {
   const [books, setBooks] = useState([])
 
+  const deleteBookById = id => {
+    const updatedBooks = books.filter(book => {
+      return book.id !== id
+    })
+
+    setBooks(updatedBooks)
+  }
+
   const createBook = title => {
     const updatedBooks = [
       ...books,
-      { id: Math.round(Math.random() * 9999), title: title },
+      { id: Math.round(Math.random() * 9999), title: title }
     ]
     setBooks(updatedBooks)
   }
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   )
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,7 +1,20 @@
 import React from "react"
 
-function BookShow({ book }) {
-  return <div className="book-show">{book.title}</div>
+function BookShow({ book, onDelete }) {
+  const handleClick = () => {
+    onDelete(book.id)
+  }
+
+  return (
+    <div className="book-show">
+      {book.title}
+      <div className="actions">
+        <button className="delete" onClick={handleClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default BookShow
