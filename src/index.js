f19c9a7f9c3884dd320f8231b88af7c4976b1dd1
Lawrence O. Caulker <locaulker@gmail.com>
2022-11-27 08:12:20 -0500

    resume from L134

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react"
 import "./index.css"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import { Provider } from "./context/books"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider value={10}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
